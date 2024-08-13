26ee8f75f4c5bd85ae8e3c7868fc8c098beded13
kasim23 <qsudais@gmail.com>
2024-08-12 21:09:08 -0400

    153-Medium-Find-Minimum-In-Rotated-Sorted-Array

---
+++ Binary-Search/153.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        
+        while left < right:
+            mid = (left+right)//2
+            if nums[mid]>nums[right]:
+                left = mid+1
+            else:
+                right = mid
+        return nums[left]
+    
+s = Solution()
+print(s.findMin([4,5,6,7,0,1,2])) 

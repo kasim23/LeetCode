7ba9d1b0bd30e696bd1c8c383e3fcd1fad6d957f
kasim23 <qsudais@gmail.com>
2025-03-02 23:23:45 -0500

    fns template

--- Graphs/graph_adj_matrix.py
+++ Graphs/graph_adj_matrix.py
@@ -3,3 +3,44 @@ class AdjMatrixGraph:
         # Create an n x n matrix filled with zeros,
         # where n is the number of vertices.
         self.adj_matrix = [[0 for _ in range(len(vertices))] for _ in range(len(vertices))]
+        
+        
+        
+    def add_vertex(self, vertex):
+        pass
+    
+    
+    def add_edge(self, u, v, directed=True):
+        pass
+    
+    
+    def remove_vertex(self, vertex):
+        pass
+    
+    
+    def remove_edge(self, u, v, directed=True):
+        pass
+    
+    
+    def has_edge(self, u, v):
+        pass
+    
+    
+    def get_neighbors(self, vertex):
+        pass
+    
+    
+    def __str__(self):
+        pass
+    
+    
+    def recursive_dfs(self, vertex, visited=None):
+        pass
+    
+    
+    def iterative_dfs(self, vertex, visited=None):
+        pass
+    
+    
+    def bfs(self, vertex, visited=None):
+        pass
